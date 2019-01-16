55e8e955273b1d50485e38b717f0ffa7c8936978
Lee Sang Hun <zasfe@users.noreply.github.com>
2019-01-16 13:22:49 +0900

    Update systeminfo.sh

--- bash/systeminfo.sh
+++ bash/systeminfo.sh
@@ -7,12 +7,12 @@ LANG=C
 # system status
 ## Os - info
 os_hostname=`hostname`
-echo -e "type=os_hostname;value=\033[32m${os_hostname}\033[0m;";
+echo -e "hostname: \033[32m${os_hostname}\033[0m";
 
 ## Hardware
 hw_vendor=`dmidecode | grep Vendor | head -n 1 | awk -F':' '{gsub(/^[ \t]+/, "", $2); print $2}'  | awk '{gsub(/^[ \t]+/, "", $1);print $1}'`
 hw_model=`dmidecode | grep "Product\ Name" | head -n 1 | awk -F':' '{gsub(/^[ \t]+/, "", $2); gsub(/[ \t]+$/, "", $2);print $2} '`
-echo -e "type=hw_model;value=\033[32m${hw_vendor} ${hw_model}\033[0m;";
+echo -e "hw: \033[32m${hw_vendor} ${hw_model}\033[0m";
 
 ## OS
 if [ -f '/etc/os-release' ] ; then
@@ -26,7 +26,7 @@ else
 fi
 [ "$os_release" == "" ] && os_release="Unknown"
 os_arch=`arch`
-echo -e "type=os;value=\033[32m${os_release} (${os_arch})\033[0m;";
+echo -e "os: \033[32m${os_release} (${os_arch})\033[0m";
 
 
 
@@ -39,7 +39,7 @@ ps aufx | egrep "(httpd|apache)" | grep -v '\\' | grep -v "org.apache" |  awk '{
 
   if [ -f "${apache_bin}" ]; then
     apache_version=`${apache_bin} -V | grep "^Server\ version" | awk -F':' '{gsub(/^[ \t]+/, "", $2); print $2}'`;
-    echo -e "type=apache_version;value=\033[32m${apache_version}\033[0m;";
+    echo -e "apache: \033[32m${apache_version}\033[0m";
   fi
 done
     
@@ -51,10 +51,10 @@ ps aufxww | grep java | grep -v grep | while read line; do
     tomcat_base=`echo "$line" | sed -e 's/\ /\n/g' | grep "^-Dcatalina.base" | awk -F\= '{print$2}'`
     if [ -n "${tomcat_base}" ]; then
       tomcat_version=`exec ${java_bin} -cp ${tomcat_base}/lib/catalina.jar org.apache.catalina.util.ServerInfo | grep "^Server\ version\:" | awk -F':' '{gsub(/^[ \t]+/, "", $2); print $2}'`;
-      echo -e "type=tomcat_version;value=\033[32mtomcat/${tomcat_version}\033[0m;value2=${tomcat_base};";
+      echo -e "tomcat: \033[32mtomcat/${tomcat_version}\033[0m ( ${tomcat_base} )";
       
       java_version=`exec ${java_bin} -cp ${tomcat_base}/lib/catalina.jar org.apache.catalina.util.ServerInfo | grep "^JVM\ Version\:" | awk -F':' '{gsub(/^[ \t]+/, "", $2); print $2}'`;
-      echo -e "type=java_version;value=\033[32mjava/${java_version}\033[0m;value2=${java_bin};";
+      echo -e "java: \033[32mjava/${java_version}\033[0m ( ${java_bin} )";
     fi
   fi
 done
@@ -65,7 +65,7 @@ ps aufx | grep mysqld | grep -v grep | grep -v mysqld_safe | awk '{print$12}' | 
   if [ `strings ${mysql_bin} | grep  "\-MariaDB$" | wc -l` -eq 1 ]; then
     mysql_version=`strings ${mysql_bin} | grep  "\-MariaDB$" | awk -F'-' '{print "MariaDB/"$1}'`
   fi
-  echo -e "type=mysql_version;value=\033[32m${mysql_version}\033[0m;value2=${mysql_bin};";
+  echo -e "mysql: \033[32m${mysql_version}\033[0m ( ${mysql_bin} )";
 done
 
 ## DBMS - oracle
@@ -77,7 +77,7 @@ ps aufx | grep tnslsnr | grep -v grep | awk '{print$11}' | uniq | while IFS= rea
   if [ -f "${oracle_inventory}" ]; then
     oracle_version=`cat ${oracle_home}/inventory/ContentsXML/comps.xml | grep oracle.server | head -n 1 | cut -d'"' -f4`;
   fi
-  echo -e "type=oracle_version;value=\033[32moracle/${oracle_version}\033[0m;value2=${oracle_home};";
+  echo -e "oracle: \033[32moracle/${oracle_version}\033[0m ( ${oracle_home} )";
 done
 
 
@@ -101,7 +101,7 @@ else
     bbname=`cat /home/bb/bb17b4/etc/bbaliasname | head -n 1`
   fi
 fi
-echo "type=monitoring_bb;value=${bbcheck};value2=${bbproc};value3=${bbname}";
+echo -e "monitoring_bb: \033[32m${bbcheck}\033[0m ( proc config: \033[32m${bbproc}\033[0m , bbhostname: ${bbname} )";
 
 ## Monitoring - zenius
 icheck=`ps aufxww | grep zagent | grep -v grep | wc -l`
@@ -110,7 +110,7 @@ if [ $icheck -eq "0" ]; then
 else
   zeniuscheck="O";
 fi
-echo "type=monitoring_zenius;value=${zeniuscheck};";
+echo -e "monitoring_zenius: \033[32m${zeniuscheck}\033[0m";
 
 
 ## Monitoring - consignClient
@@ -120,7 +120,7 @@ if [ $icheck -eq "0" ]; then
 else
   consigncheck="O";
 fi
-echo "type=monitoring_consign;value=${consigncheck};";
+echo -e "monitoring_consign: 033[32m${consigncheck}\033[0m";
 
 ## config - arp
 ip_gateway=`ip r | grep default | cut -d' ' -f3 | head -n 1`
@@ -130,7 +130,7 @@ if [ $icheck -eq "0" ]; then
 else
   arpcheck="O";
 fi
-echo "type=arp_static;value=${arpcheck};value2=${ip_gateway}";
+echo -e "cfg_arpstatic: 033[32m${arpcheck}\033[0m ( gateway ip: ${ip_gateway} )";
 
 
 
@@ -140,7 +140,6 @@ if [ "$hw_vendor" == "HP" ]; then
   [ -f '/usr/sbin/hpacucli' ] && HP_CMD='/usr/sbin/hpacucli'
   if [ "${HP_CMD}" == "" ]; then
     raidcheck="-";
-    echo "type=disk_array;value=${raidcheck};"
   else
     hp_slot_no=`$HP_CMD ctrl all show status | grep -i slot | awk -F'Slot' '{print$2}' | awk '{print$1}'`;
     if [ -n ${hp_slot_no} ]; then
@@ -150,10 +149,8 @@ if [ "$hw_vendor" == "HP" ]; then
       else
         raidcheck="X";
       fi
-      echo "type=disk_array;value=${raidcheck};"
     else
       raidcheck="-";
-      echo "type=disk_array;value=${raidcheck};"  
     fi
   fi
 elif [ "$hw_vendor" == "IBM" ] || [ "$hw_vendor" == "Dell" ]; then
@@ -162,13 +159,12 @@ elif [ "$hw_vendor" == "IBM" ] || [ "$hw_vendor" == "Dell" ]; then
 
   if [ "${IBM_CMD}" == "" ]; then
     raidcheck="-";
-    echo "type=disk_array;value=${raidcheck};"
   else
     ${IBM_CMD} -LDPDinfo -aALL -NoLog | grep . | sed -e "s/^[\t ]*//g" | egrep "^RAID\ Level|^PD|^Raw\ Size" | sed -e 's/\,/\:/g' -e 's/\[/\:/g' |  awk -F':' '{gsub(/[ \t]+/, "", $2);print $1":"$2}' | sed ':a;N;$!ba;s/\n/ /g' | sed -e 's/RAID/\nRAID/g' | grep .
   fi
 else
   raidcheck="-";
-  echo "type=disk_array;value=${raidcheck};"
-fi
 
+fi
+echo -e "disk_array: 033[32m${raidcheck}\033[0m"
 
