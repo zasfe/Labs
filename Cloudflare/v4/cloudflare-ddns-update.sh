2cc3e8f30bfd6eb6cf1c5cc8a2514b172356b734
Lee Sang Hun <zasfe@users.noreply.github.com>
2022-02-28 09:45:34 +0900

    Create cloudflare-ddns-update.sh

---
+++ Cloudflare/v4/cloudflare-ddns-update.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# original url: https://gist.github.com/Tras2/cba88201b17d765ec065ccbedfb16d9a
+
+# A bash script to update a Cloudflare DNS A record with the external IP of the source machine
+# Used to provide DDNS service for my home
+# Needs the DNS record pre-creating on Cloudflare
+
+# Proxy - uncomment and provide details if using a proxy
+#export https_proxy=http://<proxyuser>:<proxypassword>@<proxyip>:<proxyport>
+
+# Cloudflare zone is the zone which holds the record
+zone=example.com
+# dnsrecord is the A record which will be updated
+dnsrecord=www.example.com
+
+## Cloudflare authentication details
+## keep these private
+cloudflare_auth_email=me@cloudflare.com
+cloudflare_auth_key=1234567890abcdef1234567890abcdef
+
+
+# Get the current external IP address
+ip=$(curl -s -X GET https://checkip.amazonaws.com)
+
+echo "Current IP is $ip"
+
+if host $dnsrecord 1.1.1.1 | grep "has address" | grep "$ip"; then
+  echo "$dnsrecord is currently set to $ip; no changes needed"
+  exit
+fi
+
+# if here, the dns record needs updating
+
+# get the zone id for the requested zone
+zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone&status=active" \
+  -H "X-Auth-Email: $cloudflare_auth_email" \
+  -H "X-Auth-Key: $cloudflare_auth_key" \
+  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
+
+echo "Zoneid for $zone is $zoneid"
+
+# get the dns record id
+dnsrecordid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$dnsrecord" \
+  -H "X-Auth-Email: $cloudflare_auth_email" \
+  -H "X-Auth-Key: $cloudflare_auth_key" \
+  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
+
+echo "DNSrecordid for $dnsrecord is $dnsrecordid"
+
+# update the record
+curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsrecordid" \
+  -H "X-Auth-Email: $cloudflare_auth_email" \
+  -H "X-Auth-Key: $cloudflare_auth_key" \
+  -H "Content-Type: application/json" \
+  --data "{\"type\":\"A\",\"name\":\"$dnsrecord\",\"content\":\"$ip\",\"ttl\":1,\"proxied\":false}" | jq
