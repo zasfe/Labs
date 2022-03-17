67cc48e2531e05df96c601601ad5b5d4c9189165
Lee Sang Hun <zasfe@users.noreply.github.com>
2022-03-17 12:22:58 +0900

    Update import-image-os.sh

--- Openstack/import-image-os.sh
+++ Openstack/import-image-os.sh
@@ -2,8 +2,8 @@
 
 source /root/keystonerc_admin
 
-mkdir -p /home/image/CentOS
-cd /home/image/CentOS
+mkdir -p /home/images/CentOS
+cd /home/images/CentOS
 wget http://cloud.centos.org/centos/6/images/CentOS-6-x86_64-GenericCloud.qcow2
 openstack image create "CentOS-6-x86_64" --file ./CentOS-6-x86_64-GenericCloud.qcow2 --disk-format qcow2 --container-format bare --public
 
@@ -11,8 +11,8 @@ wget https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2
 openstack image create "CentOS-7-x86_64" --file ./CentOS-7-x86_64-GenericCloud.qcow2 --disk-format qcow2 --container-format bare --public
 
 
-mkdir -p /home/image/Ubuntu
-cd /home/image/Ubuntu
+mkdir -p /home/images/Ubuntu
+cd /home/images/Ubuntu
 wget https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img
 openstack image create "Ubuntu 14.04.5 LTS (Trusty Tahr)" --file ./trusty-server-cloudimg-amd64-disk1.img --disk-format qcow2 --container-format bare --public
 
@@ -25,3 +25,7 @@ openstack image create "Ubuntu 18.04 LTS (Bionic Beaver)" --file ./bionic-server
 wget https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
 openstack image create "Ubuntu 20.04 LTS (Focal Fossa)" --file ./focal-server-cloudimg-amd64.img --disk-format qcow2 --container-format bare --public
 
+mkdir -p /home/images/Ubuntu
+cd /home/images/Ubuntu
+wget http://download.cirros-cloud.net/0.5.2/cirros-0.5.2-x86_64-disk.img
+openstack image create "cirros" --file cirros-0.5.2-x86_64-disk.img --disk-format qcow2 --container-format bare --public
