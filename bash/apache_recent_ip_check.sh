14a2ef4dbb83c9ba79890a8710d0ef62dd449775
Lee Sang Hun <zasfe@users.noreply.github.com>
2022-05-11 19:19:03 +0900

    Update apache_recent_ip_check.sh

--- bash/apache_recent_ip_check.sh
+++ bash/apache_recent_ip_check.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-# Apache Log File
-file_apachelog="/usr/local/apache/logs/ssl_custom_log"
+LANG=C
 
-echo -e "\033[34m # 최근 1000개 접속 중 최다 접근 IP 상위 10개 추출 \033[0m"
+# Apache Log File
+file_apachelog="/usr/local/apache/logs/access_log"
+
+if [ ! $1 == "" ]; then
+    if [ -f $1 ]; then
+        file_apachelog=$1
+    else
+        echo ""
+        echo -e "\033[34m # Log file Not Exists \033[0m"
+        echo " $1"
+        echo ""
+    endif
+endif
+
+echo ""
+echo -e "\033[34m # Extract the top 10 most accessed IPs out of the last 1000 connections \033[0m"
 tail -n 10000 ${file_apachelog} | awk '{print $1}' | sort | uniq -c | sort -nr | head -n 10
 echo ""
 
-echo -e "\033[34m # 접속이 많은 URL 상위 10개 추출 \033[0m"
+echo -e "\033[34m # Extract the top 10 most visited URLs \033[0m"
 tail -n 10000 ${file_apachelog} | awk '{print $7}' | sort | uniq -c | sort -nr | head -n 10
 echo ""
 
-echo -e "\033[34m # 최대 접근 IP 5개 별 최다 접근 URL 추출 \033[0m"
+echo -e "\033[34m # Extract the most accessed URLs for each of the 5 maximum access IPs \033[0m"
 echo ""
 tail -n 10000 ${file_apachelog} | awk '{print $1}' | sort | uniq -c | sort -nr | head -n 5 | while IFS= read LINE ; do
    log_ip=`echo $LINE | awk '{print$2}'`;
