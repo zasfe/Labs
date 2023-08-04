fc0661ad62ae55f758ce55d7e580c30a6b784d95
Lee Sang Hun <zasfe@users.noreply.github.com>
2023-08-04 14:40:03 +0900

    Update worker_node.sh

--- docker/k8s/study/worker_node.sh
+++ docker/k8s/study/worker_node.sh
@@ -8,4 +8,6 @@ ping -c 3 127.0.0.1 >/dev/null 2>&1
 
 # config for work_nodes only 
 kubeadm join --token 123456.1234567890123456 \
-             --discovery-token-unsafe-skip-ca-verification 192.168.1.10:6443 --v=5
+             --discovery-token-unsafe-skip-ca-verification 192.168.1.10:6443 \
+             --ignore-preflight-errors=all \
+             --v=5 
