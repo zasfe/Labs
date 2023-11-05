f8ad39cfcf18630dff4373ad7ceb3d204e080e0f
Lee Sang Hun <zasfe@users.noreply.github.com>
2023-11-05 21:26:51 +0900

    Update info.sh

--- bash/info.sh
+++ bash/info.sh
@@ -46,8 +46,8 @@ if [ -f ${HOME}/managed ]; then
         cat ${HOME}/managed
         echo ""
 fi
-if [ -f ${HOME}/service_check.sh ]; then
-        /bin/bash ${HOME}/service_check.sh
+if [ -f ${HOME}/serviceport_check.sh ]; then
+        /bin/bash ${HOME}/serviceport_check.sh
 fi
 echo -e "\033[34m ========================================================================= \033[0m    "
 echo ""
