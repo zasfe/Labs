95a2be973abe9be13675da2d2ef78323b446a8e4
Lee Sang Hun <zasfe@users.noreply.github.com>
2024-02-06 18:00:52 +0900

    Update install-nginder.sh

--- nGrinder/install-nginder.sh
+++ nGrinder/install-nginder.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-# Docker Swarm node check
+## Docker Swarm node check
 docker node ls
 
-# Docker Swarm network create
+## Docker Swarm network create
 docker network create -d overlay --attachable ngrinder-nw
 
-# ngrinder container run
+## nGrinder container run
 docker run -d --restart=unless-stopped -v ~/ngrinder-controller:/opt/ngrinder-controller --name controller -p 8000:80 --network ngrinder-nw ngrinder/controller
 
 
-# ngrinder agent
+## nGrinder agent
 docker service rm agent
 docker service create --name agent --network ngrinder-nw --replicas 4 ngrinder/agent 
 
-# ngrinter manager node drain
+## nGrinter manager node drain
 # docker node update --availability drain NODENAME
+# docker node update --availability drain `hostname`
 
-
+## HowTo nGrinder agent scale out 12
 # docker service scale agent=12
 
 
