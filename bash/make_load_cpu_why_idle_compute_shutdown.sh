a776fc4f38fdec058cdf3e3189413d001291b60c
Lee Sang Hun <zasfe@users.noreply.github.com>
2024-02-07 17:54:30 +0900

    Update make_load_cpu_why_idle_compute_shutdown.sh

--- bash/make_load_cpu_why_idle_compute_shutdown.sh
+++ bash/make_load_cpu_why_idle_compute_shutdown.sh
@@ -5,25 +5,18 @@
 # Memory: 90% on Total
 # Time(s): 500
 
-sudo stress --cpu 1 --vm-bytes $(awk '/MemAvailable/{printf "%d\n", $2 * 0.9;}' < /proc/meminfo)k --timeout 500
+# sudo stress --cpu 1 --vm-bytes $(awk '/MemAvailable/{printf "%d\n", $2 * 0.9;}' < /proc/meminfo)k --timeout 500
+sudo nohup stress --cpu 2 --vm-bytes $(awk '/MemAvailable/{printf "%d\n", $2 * 0.1;}' < /proc/meminfo)k >/dev/null 2>&1 &
 
 
-# CentOS
+# [ CentOS ]
 # sudo yum -y install epel-release; 
 # sudo yum -y install stress;
 # wget https://raw.githubusercontent.com/zasfe/Labs/master/bash/load_cpu_why_idle_compute_shutdown.sh
 # chmod +x ./load_cpu_why_idle_compute_shutdown.sh
-# echo "" >> /etc/crontab
-# echo "# Load" >> /etc/crontab
-# echo "0 5 * * * * opc /home/opc/load_cpu_why_idle_compute_shutdown.sh" >> /etc/crontab
-# sudo systemctl restart crond
 
-# Ubuntu
+# [ Ubuntu ]
 # sudo apt-get install -y stress
 # wget https://raw.githubusercontent.com/zasfe/Labs/master/bash/load_cpu_why_idle_compute_shutdown.sh
 # chmod +x ./load_cpu_why_idle_compute_shutdown.sh
-# echo "" >> /etc/crontab
-# echo "# Load" >> /etc/crontab
-# echo "0 5 * * * * ubuntu /home/ubuntu/load_cpu_why_idle_compute_shutdown.sh" >> /etc/crontab
-# sudo systemctl restart cron
 
