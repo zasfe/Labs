4a953e3bb013c0f24215096f92564a0ef9d9bcfc
Lee Sang Hun <zasfe@users.noreply.github.com>
2024-04-01 19:19:05 +0900

    Create gitlab-runner-register.sh

---
+++ gitlab/gitlab-runner-register.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+###################################################################
+# Récuperer le token d'enregistrement du runner via ce lien:
+# http://localhost:8080/root/${project}/settings/ci_cd
+# Benoit Petit: https://github.com/benoitpetit
+###################################################################
+
+# modifier avec votre token
+registration_token=XXXXXXXXXXXXXXX
+url=http://127.0.0.1
+
+docker exec -it gitlab-runner1 \
+  gitlab-runner register \
+    --non-interactive \
+    --registration-token ${registration_token} \
+    --locked=false \
+    --description docker-stable \
+    --url ${url} \
+    --executor docker \
+    --docker-image docker:stable \
+    --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
+    --docker-network-mode gitlab-network
+    
+# executer le script pour inscrire le runner dans Gitlab
