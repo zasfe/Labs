d2e4e2b84fdb1d8bb0eeba03a735962265c6e289
zasfe <zasfe@users.noreply.github.com>
2024-05-27 14:14:20 +0900

    Update check_custom02.sh

--- bash/check_custom02.sh
+++ bash/check_custom02.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p /var/log/syslog/
+
 #생성 파일
 log_file="/var/log/syslog/`date +%Y%m%d`"
 
@@ -20,13 +22,13 @@ function syscheck() {
         uptime >> $log_file
 #       top -c >> $log_file
 #      pstree |grep mysqld >> $log_file
-       echo "====ps tree====" >> $log_file
+       echo "==== pstree ====" >> $log_file
         pstree |grep httpd >> $log_file
 #        ps awwux >> $log_file
-       echo "====free -m====" >> $log_file
+       echo "==== free -m ====" >> $log_file
         free -m >> $log_file
 
-        echo "====netstat countdown====" >> $log_file
+        echo "==== netstat countdown ====" >> $log_file
         echo " - TCP/80: `netstat -ano |grep 80 |wc -l`" >> $log_file
         echo " - TCP/443: `netstat -ano |grep 443 |wc -l`" >> $log_file
         echo " - TCP/3306: `netstat -ano |grep 3306 |wc -l`" >> $log_file
@@ -38,10 +40,10 @@ function syscheck() {
         echo " - TCP/443(ESTABLISHED/TIME_WAIT): `netstat -ano |grep 443 | grep ESTABLISHED |wc -l` / `netstat -ano |grep 443 | grep TIME_WAIT |wc -l`" >> $log_file
         echo " - TCP/3306(ESTABLISHED/TIME_WAIT): `netstat -ano |grep 443 | grep ESTABLISHED |wc -l` / `netstat -ano |grep 443 | grep TIME_WAIT |wc -l`" >> $log_file
 
-        echo " ==== top ==== "  >> $log_file
+        echo "==== top ==== "  >> $log_file
         top -b -n 1 >> $log_file
 
-        echo "====netstat====" >> $log_file
+        echo "==== netstat ====" >> $log_file
         netstat -nltp  >> $log_file
         netstat -ano |grep EST >> $log_file
         netstat -ano| grep TIME_WAIT  >> $log_file
