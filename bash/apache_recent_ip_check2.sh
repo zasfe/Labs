bacc009b023104ac57f9a352152d5883bb2f23f3
zasfe <zasfe@users.noreply.github.com>
2024-07-17 14:43:11 +0900

    Update apache_recent_ip_check2.sh

--- bash/apache_recent_ip_check2.sh
+++ bash/apache_recent_ip_check2.sh
@@ -15,7 +15,8 @@ function return_top10_most_url_on_accesslog {
   FILE=$1
   if [ -f "$FILE" ]; then
     echo -e "\033[34m  ## Extract the top 10 most visited URLs \033[0m"
-    tail -n 10000 ${FILE} | awk '{print $7}' | sort | uniq -c | sort -nr | head -n 10;
+    column_url=`head -n 1 ${FILE} | awk -v b="\"GET" '{for (i=1;i<=NF;i++) { if ($i == b) { print i+1 } }}'`
+    tail -n 10000 ${FILE} | awk -v col=${column_url} '{print $col}' | sort | uniq -c | sort -nr | head -n 10;
   else
     echo "$FILE does not exist.";
   fi
@@ -25,12 +26,13 @@ function return_top10_most_url_max10_ip_on_accesslog {
   FILE=$1
   if [ -f "$FILE" ]; then
     echo -e "\033[34m  ## Extract the most accessed URLs for each of the 5 maximum access IPs \033[0m";
+    column_url=`head -n 1 ${FILE} | awk -v b="\"GET" '{for (i=1;i<=NF;i++) { if ($i == b) { print i+1 } }}'`
     tail -n 10000 ${FILE} | awk '{print $1}' | sort | uniq -c | sort -nr | head -n 5 | while IFS= read LINE ; do
       log_ip=`echo $LINE | awk '{print$2}'`;
       log_count=`echo $LINE | awk '{print$1}'`;
-      
-      echo -e "\033[34m # IP: ${log_ip} / ${log_count} \033[0m";
-      tail -n 10000 ${file_apachelog} | grep ${log_ip} | awk '{print $7}' | sort | uniq -c | sort -nr | head -n 5;
+
+      echo -e "\033[34m  ## IP: ${log_ip} / ${log_count} \033[0m";
+      tail -n 10000 ${FILE} | grep ${log_ip} | awk -v col=${column_url} '{print $col}' | sort | uniq -c | sort -nr | head -n 5;
     done
   else
     echo "$FILE does not exist.";
@@ -51,4 +53,5 @@ date_log="$(date "+%Y%m%d").log"
 
 echo " ------- [ Service Check ] ----------------------------------------------  "
 # echo " $(print_portcheck 'localhost - http' /var/log/httpd/access-${date_log})"
+echo " $(print_portcheck 'localhost - http' /var/log/httpd/access_log)"
 echo " ------------------------------------------------------------------------- "
