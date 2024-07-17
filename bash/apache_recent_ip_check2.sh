b24c225866eb97e0f488d668e63685704c4cb3ee
zasfe <zasfe@users.noreply.github.com>
2024-07-17 14:10:33 +0900

    Update apache_recent_ip_check2.sh

--- bash/apache_recent_ip_check2.sh
+++ bash/apache_recent_ip_check2.sh
@@ -15,7 +15,7 @@ function return_top10_most_url_on_accesslog {
   FILE=$1
   if [ -f "$FILE" ]; then
     echo -e "\033[34m  ## Extract the top 10 most visited URLs \033[0m"
-    tail -n 10000 ${FILE} | awk '{print $7}' | sort | uniq -c | sort -nr | head -n 10
+    tail -n 10000 ${FILE} | awk '{print $7}' | sort | uniq -c | sort -nr | head -n 10;
   else
     echo "$FILE does not exist.";
   fi
@@ -24,13 +24,13 @@ function return_top10_most_url_on_accesslog {
 function return_top10_most_url_max10_ip_on_accesslog {
   FILE=$1
   if [ -f "$FILE" ]; then
-    echo -e "\033[34m  ## Extract the most accessed URLs for each of the 5 maximum access IPs \033[0m"
+    echo -e "\033[34m  ## Extract the most accessed URLs for each of the 5 maximum access IPs \033[0m";
     tail -n 10000 ${FILE} | awk '{print $1}' | sort | uniq -c | sort -nr | head -n 5 | while IFS= read LINE ; do
       log_ip=`echo $LINE | awk '{print$2}'`;
       log_count=`echo $LINE | awk '{print$1}'`;
       
       echo -e "\033[34m # IP: ${log_ip} / ${log_count} \033[0m";
-      tail -n 10000 ${file_apachelog} | grep ${log_ip} | awk '{print $7}' | sort | uniq -c | sort -nr | head -n 5
+      tail -n 10000 ${file_apachelog} | grep ${log_ip} | awk '{print $7}' | sort | uniq -c | sort -nr | head -n 5;
     done
   else
     echo "$FILE does not exist.";
@@ -39,15 +39,16 @@ function return_top10_most_url_max10_ip_on_accesslog {
 
 function print_portcheck {
   if ! [[ "$1" == "" || "$2" == "" ]]; then
-    echo -e "\033[32m  # $1 \033[0m "
-    echo "$(return_top10_most_ips_on_accesslog $2)"
-    echo "$(return_top10_most_url_on_accesslog $2)"
-    echo "$(return_top10_most_url_max10_ip_on_accesslog $2)"
+    echo -e "\033[32m  # $1 \033[0m ";
+    echo "$(return_top10_most_ips_on_accesslog $2)";
+    echo "$(return_top10_most_url_on_accesslog $2)";
+    echo "$(return_top10_most_url_max10_ip_on_accesslog $2)";
+    echo "";
   fi
 }
 
 date_log="$(date "+%Y%m%d").log"
 
 echo " ------- [ Service Check ] ----------------------------------------------  "
-echo " $(print_portcheck 'localhost - http' /var/log/httpd/access-${date_log})"
+# echo " $(print_portcheck 'localhost - http' /var/log/httpd/access-${date_log})"
 echo " ------------------------------------------------------------------------- "
