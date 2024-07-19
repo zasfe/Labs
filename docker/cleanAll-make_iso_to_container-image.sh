7f5b4692175673e9d764330067bc554c0abaf78d
zasfe <zasfe@users.noreply.github.com>
2024-07-19 18:00:09 +0900

    Update cleanAll-make_iso_to_container-image.sh

--- docker/cleanAll-make_iso_to_container-image.sh
+++ docker/cleanAll-make_iso_to_container-image.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 LANG=C
 
-umount -f `df -Ph | egrep "^\/.*rootfs$" | awk '{print$1}'`
+MOUNT_POINT=`df -Ph | egrep "^\/.*rootfs$" | awk '{print$1}'`;
+
+if test -z "${MOUNT_POINT}" 
+then
+    echo "# Not Find Mount point"
+else
+    echo "# Umount Mount point :${MOUNT_POINT} "
+    umount -f ${MOUNT_POINT}
+fi
+
 test -d ./rootfs && rm -rf ./rootfs
 test -d ./unsquashfs && rm -rf ./unsquashfs
