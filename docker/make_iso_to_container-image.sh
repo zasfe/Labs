7dce340b93ca13134e07ff33b4b1069169911515
zasfe <zasfe@users.noreply.github.com>
2024-07-19 20:54:44 +0900

    Update make_iso_to_container-image.sh

--- docker/make_iso_to_container-image.sh
+++ docker/make_iso_to_container-image.sh
@@ -6,8 +6,28 @@ ISO_URL="https://releases.ubuntu.com/18.04.6/ubuntu-18.04.6-live-server-amd64.is
 
 echo "# STEP1 ::: download iso file, read only filesystem tools";
 ISO_FILE=`echo ${ISO_URL} | awk -F "/" '{print $NF}'`;
-ISO_OS_NAME=`echo ${ISO_FILE} | awk -F "-" '{print $1"-"$2}' | awk '{print tolower($0)}'`;
 ISO_OS_VERSION=`echo ${ISO_FILE} | awk -F "-" '{print $1"-"$2}' | awk '{print tolower($0)}'`;
+ISO_OS_NAME=`echo ${ISO_FILE} | awk -F "-" '{print $1}' | awk '{print tolower($0)}'`;
+delimiter="-"
+
+# 첫 번째 구분자 위치 찾기
+position=$(expr index "$ISO_FILE" "$delimiter");
+
+# 구분자 이후 문자열 출력
+ISO_VERSION=${input:$position};
+
+
+input="aa-bb-cc"
+delimiter="-"
+
+# 첫 번째 구분자 위치 찾기
+position=$(expr index "$input" "$delimiter")
+
+# 구분자 이후 문자열 출력
+result=${input:$position}
+echo "$result"
+
+
 
 wget -O ${ISO_FILE} ${ISO_URL};
 apt-get install -y squashfs-tools;
@@ -23,18 +43,18 @@ ISO_ROOTFS=`find . -type f | grep 'filesystem\.squashfs$'`;
 test -f ${ISO_ROOTFS} && unsquashfs -f -d unsquashfs/ ${ISO_ROOTFS};
 
 echo "# STEP3 ::: make container images";
-tar -C unsquashfs -c . | docker import - zasfe/${ISO_OS_NAME}:latest
+tar -C unsquashfs -c . | docker import - zasfe/${ISO_OS_VERSION}:latest
 
 echo "# STEP4 ::: container running test";
-docker exec --rm -h myos -t zasfe/${ISO_OS_NAME}:latest cat /etc/os-release | grep -i PRETTY_NAME
-echo "docker run -it --rm -h myos -t zasfe/${ISO_OS_NAME}:latest bash"
+docker exec --rm -h myos -t zasfe/${ISO_OS_VERSION}:latest cat /etc/os-release | grep -i PRETTY_NAME
+echo "docker run -it --rm -h myos -t zasfe/${ISO_OS_VERSION}:latest bash"
 
 
 echo "# STEP5 ::: container image push";
 echo "ex)";
-echo " - (local only) docker image name : zasfe/${ISO_OS_NAME}";
-echo " - (remote only) docker image name : zasfe/ubuntu:18.04.6-live-server-amd64.iso";
+echo " - (local only) docker image name : zasfe/${ISO_OS_VERSION}:latest ";
+echo " - (remote only) docker image name : zasfe/${ISO_OS_NAME}:${ISO_VERSION}";
 echo "";
-echo "docker tag zasfe/ubuntu-18.04.6:latest zasfe/ubuntu:18.04.6-live-server-amd64.iso";
-echo "docker push zasfe/ubuntu:18.04.6-live-server-amd64.iso";
+echo "docker tag zasfe/${ISO_OS_VERSION}:latest zasfe/${ISO_OS_NAME}:${ISO_VERSION}";
+echo "docker push zasfe/${ISO_OS_NAME}:${ISO_VERSION}";
 echo "";
