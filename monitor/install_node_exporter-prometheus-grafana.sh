f9cfd0d6e31c12c5de16aed78bffc7f297b43a3e
zasfe <zasfe@users.noreply.github.com>
2024-07-23 17:21:00 +0900

    Update install_node_exporter-prometheus-grafana.sh

--- monitor/install_node_exporter-prometheus-grafana.sh
+++ monitor/install_node_exporter-prometheus-grafana.sh
@@ -163,3 +163,4 @@ systemctl restart grafana-server.service
 # Adding datasource without using the web gui (https://github.com/grafana/grafana/issues/1789)
 curl --user admin:admin ' http://127.0.0.1:3000/api/datasources ' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"prometheusLocal","isDefault":true ,"type":"prometheus","url":" http://127.0.0.1:9090","access":"proxy","basicAuth":false} '
 
+# grafana-server web UI: http://SERVER-NAME:3000/
