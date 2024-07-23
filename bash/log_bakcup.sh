c695606f34f0b1fcfadfe47b83d440628ab9be17
zasfe <zasfe@users.noreply.github.com>
2024-07-23 09:22:29 +0900

    Create log_bakcup.sh

---
+++ bash/log_bakcup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# - log archive
+
+folder_logs="/data/logs"
+  
+# 30 Days old file compressed
+find ${folder_backup} -maxdepth 1 -type f -name "*" -and ! -name "*.gz" -mtime +30 -print | xargs gzip 
+
+exit 0
