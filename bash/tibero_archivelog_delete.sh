7a3982ce2dc60f64c8105dd35c18bcf882bfb126
zasfe <zasfe@users.noreply.github.com>
2024-07-26 15:08:34 +0900

    Update tibero_archivelog_delete.sh

--- bash/tibero_archivelog_delete.sh
+++ bash/tibero_archivelog_delete.sh
@@ -5,7 +5,12 @@ LANG=C
 # Archive Log File은 주기적/지속적으로 생성되는 파일이므로 불필요한 경우 삭제 또는 이동합니다.
 
 export DB_ARCHIVE_LOG="/archive"
+export DB_ARCHIVE_LOG_BACKUP="/backup"
 
-find ${DB_ARCHIVE_LOG}/ -type f -name "*.arc" -mtime +30 -delete
+# 3 Days old file move
+find ${DB_ARCHIVE_LOG}/ -maxdepth 1 -type f -name "*" -mtime +14 -exec mv {} ${DB_ARCHIVE_LOG_BACKUP} \;
+
+# 366 Days old file delete
+find ${DB_ARCHIVE_LOG_BACKUP}/ -maxdepth 1 -type f -name "*" -mtime +366 -delete \;
 
 exit 0
