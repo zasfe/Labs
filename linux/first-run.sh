f7f3d202cdf8334732a9706845f9f22a929a970d
zasfe <zasfe@users.noreply.github.com>
2025-06-06 16:35:12 +0900

    Create first-run.sh

---
+++ linux/first-run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+mkdir update-cloudflare-dns; cd update-cloudflare-dns
+wget https://raw.githubusercontent.com/zasfe/Labs/refs/heads/master/Cloudflare/v4/update-cloudflare-dns.sh
+wget https://raw.githubusercontent.com/zasfe/Labs/refs/heads/master/Cloudflare/v4/update-cloudflare-dns.conf
+chmod 700 update-cloudflare-dns.sh
+
+sudo tee /etc/ssh/sshd_config.d/70-port-38317-add.conf<<EOF
+Port 22
+EOF
+
+sudo tee /etc/ssh/sshd_config.d/70-usedns-no.conf<<EOF
+UseDNS no
+EOF
+
+sudo systemctl restart sshd
+
+
