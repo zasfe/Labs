152e7d27a9ef2d81d54060e0c66a0488c72b75fe
zasfe <zasfe@users.noreply.github.com>
2025-07-01 12:54:12 +0900

    Update log_archive.sh

--- bash/log_archive.sh
+++ bash/log_archive.sh
@@ -6,4 +6,6 @@ folder_logs="/data/logs"
 # 30 Days old file compressed
 find ${folder_backup} -maxdepth 1 -type f -name "*" -and ! -name "*.gz" -mtime +30 -exec gzip -9 {} \;
 
+# 360 Days old file deleted
+find ${folder_backup} -maxdepth 1 -type f -name "*" -mtime +366 -print0 | xargs -0 rm -f
 exit 0
