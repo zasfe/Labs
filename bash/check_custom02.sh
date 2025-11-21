626413416e95fbc8d770c53e46f1d1733c09d15a
zasfe <zasfe@users.noreply.github.com>
2025-11-21 22:52:16 +0900

    Enhance logging in check_custom02.sh for system metrics
    Added additional logging for system metrics including free memory, top processes, and Docker stats.

--- bash/check_custom02.sh
+++ bash/check_custom02.sh
@@ -19,15 +19,19 @@ function syscheck() {
         echo " " >> $log_file
         w >> $log_file
 
+        echo "" >> $log_file
         echo "==== free -m ====" >> $log_file
         free -m >> $log_file
 
+        echo "" >> $log_file
         echo "==== top ==== "  >> $log_file
         top -c -b -n 1 -w  >> $log_file
 
+        echo "" >> $log_file
         echo "==== ps ====" >> $log_file
         ps aufxww >> $log_file
 
+        echo "" >> $log_file
         echo "==== pstree ====" >> $log_file
         if command -v pstree &>/dev/null; then
           pstree --ascii --long   >> $log_file
@@ -35,19 +39,30 @@ function syscheck() {
           echo "Not Found pstree" >> $log_file
         fi
 
+        echo "" >> $log_file
         echo "==== netstat ====" >> $log_file
         netstat -nltp  >> $log_file
         netstat -anop |grep EST >> $log_file
         netstat -anop | grep TIME_WAIT  >> $log_file
-
+        
+        echo "" >> $log_file
         echo "==== docker ====" >> $log_file
         if command -v docker &>/dev/null; then
           docker ps -a   >> $log_file
+          echo "" >> $log_file
           docker stats --all --no-stream --no-trunc  >> $log_file
         else
           echo "Not Found docker" >> $log_file
         fi
 
+        echo "" >> $log_file
+        echo "==== iotop ====" >> $log_file
+        if command -v iotop &>/dev/null; then
+          iotop --batch --time --iter=1 --accumulated | head -n 30  >> $log_file
+        else
+          echo "Not Found iotop" >> $log_file
+        fi
+        
 }
 
 #function mysql_processlist() {
