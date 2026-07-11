7343d332ad2913e25e9f362c7499eb3a288acfdb
zasfe <zasfe@users.noreply.github.com>
2026-07-11 18:36:17 +0900

    Modify Minecraft batch file for Forge version
    Updated Minecraft batch file to include Forge version execution.

--- TLauncher/Minecraft-1.20.1.bat
+++ TLauncher/Minecraft-1.20.1.bat
@@ -3,6 +3,11 @@ setlocal
 
 set "SCRIPT_DIR=%~dp0"
 
+powershell.exe -NoProfile -ExecutionPolicy Bypass `
+  -File ".\Start-TLauncher.ps1" `
+  -Version "Forge 1.20.1"
+
+
 powershell.exe -NoProfile -ExecutionPolicy Bypass ^
   -File "%SCRIPT_DIR%Start-TLauncher.ps1" ^
   -Version "release 1.20.1" ^
